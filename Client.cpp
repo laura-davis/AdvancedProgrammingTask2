eaab2acf750730f53f24f9faee39b34bdf05e140
laura-davis <laura.davis@sky.uk>
2021-08-21 19:26:44 +0100

    Changes made to client and server

--- Client.cpp
+++ Client.cpp
@@ -3,7 +3,6 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/socket.h>
-#include <netdb.h>
 #include <arpa/inet.h>
 #include <cstring>
 #include <string>
@@ -11,67 +10,41 @@
 using namespace std;
 
 int main() {
-    int listening = socket(AF_INET, SOCK_STREAM, 0); //	Create socket using socket function
-    // TODO - get rid? Domain = AF_INET (IPv4), Type = Stream Socket (SOCK_STREAM), Protocol = 0
-    if (listening == -1) { // If socket doesn't exist...
+    // Create a socket
+    int sock = socket(AF_INET, SOCK_STREAM, 0); //	Create socket using socket function
+    if (sock == -1) { // If unable to create socket...
         cerr << "Error creating socket" << endl; // ...Output error message
-        return -1; // ...and return error code
+        return 1; // ...and return error code
     }
-
-    sockaddr_in hint{}; // Initialise hint from the server
-    hint.sin_family = AF_INET; // Use IPv4 domain TODO - Extract this to separate class
-    hint.sin_port = htons(54000); // Convert port number from host to network short
-    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr); // Convert IP address to an array of integers
-    if (::bind(listening, (sockaddr*)&hint, sizeof(hint)) == -1) { // If unable to bind hint to port..
-        // socket, domain, pointer to socket address, length of address
-        cerr << "Error binding to port" << endl; // ...Output error message
-        return -2; // ...and return error code
-    }
-    if (listen(listening, SOMAXCONN) == -1) { // If unable to listen to socket...
-        cerr << "Error listening to port" << endl; // ...Output error message
-        return -3; // ...and return error code
-    }
-    sockaddr_in client{}; // Initialise client
-    socklen_t clientSize = sizeof(client); //
-    char host[NI_MAXHOST]; // Buffer for host
-    char svc[NI_MAXSERV]; // Buffer for service
-    int clientSocket = accept(listening, (sockaddr *) &client, &clientSize); // Accept incoming connection
-    if (clientSocket == -1) { // If unable to accept connection...
-        cerr << "Error connecting to client" << endl; // ...Output error message
-        return -4; // ...and return error code
-    }
-    close(listening); // Close listening socket
-    memset(host, 0, NI_MAXHOST); // Clean up host memory
-    memset(svc, 0, NI_MAXSERV); // Clean up service memory
-
-    int result = getnameinfo((sockaddr *) &client, sizeof(client), host, NI_MAXHOST, svc, NI_MAXSERV,
-                             0); // Convert address to computer name and location
-
-    if (result) { // If successful...
-        cout << "Host connected on " << svc << endl; // ...Output details of computer
-    } else { // Otherwise...
-        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST); // Convert network address to string
-        cout << host << " connected on " << ntohs(client.sin_port) << endl; // Print connection details
+    int port = 54000; // Port specified in client code
+    string ipAddress = "127.0.0.1"; // Local IP address
+    sockaddr_in hint{}; // Initialise hint from the client
+    hint.sin_family = AF_INET; // Use IPv4 domain
+    hint.sin_port = htons(port); // Convert port number from host to network short
+    inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr); // Convert IP address to an array of integers
+    int connectRes = connect(sock, (sockaddr *) &hint, sizeof(hint)); // Connect to remote machine
+    if (connectRes == -1) { // If unable to connect...
+        cerr << "Error connecting to server" << endl; // ...Output error message
+        return 2; // ...and return error code
     }
     char buf[4096]; // Initialise buffer
-
-    while (true) { // While buffer exists
-        memset(buf, 0, 4096); // Clear buffer
-        int bytesRecv = recv(clientSocket, buf, 4096, 0); // Initialise bytesRecv variable, ready to receive data
-        if (bytesRecv == -1) // If there is an error...
-        {
-            cout << "Error with connection" << endl; // ...Output error message
-            break;
+    string userInput; // Initialise string for capturing user input
+    do { // "Endless" loop
+        cout << "> "; // Prompt user for input
+        getline(cin, userInput); // Capture entire line of text
+        long sendRes = send(sock, userInput.c_str(), userInput.size() + 1, 0); // Send to client; add 1 for trailing 0
+        if (sendRes == -1) { // If unable to send to server...
+            cerr << "Error sending to server" << endl; // ...Output error message
+            continue; // ...and go back to try again
         }
-        if (bytesRecv == 0) { // If no data is received
-            cout << "Client disconnected" << endl; // ...Output error message
-            break;
+        memset(buf, 0, 4096); // Clear buffer and wait for response
+        if (strcmp(userInput.c_str(), "QUIT") == 0) { // If user enters "QUIT"...
+            cout << "Connection between client and server terminated" << endl; // ...Output message
+            return 0; // ...and terminate programme
         }
-        cout << "Server: " << string(buf, 0, bytesRecv) << "\r\n"; // Output message details
-        send(clientSocket, buf, bytesRecv + 1, 0); // Send message
-    }
-    close(clientSocket); // Close client socket
-
+        long bytesReceived = recv(sock, buf, 4096, 0); // Initialise bytesReceived variable, ready to receive data
+        cout << "Server: " << string(buf, bytesReceived) << "\r\n"; // Display response
+    } while (true);
+    close(sock); // Close socket
     return 0;
-
 }
--- Server.cpp
+++ Server.cpp
@@ -5,42 +5,71 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <arpa/inet.h>
-#include <string.h>
+#include <cstring>
 #include <string>
 
 using namespace std;
 
 int main() {
-    // Create a socket
-    int sock = socket(AF_INET, SOCK_STREAM, 0); //	Create socket using socket function
-    if (sock == -1) { // If unable to create socket...
+    int listening = socket(AF_INET, SOCK_STREAM, 0); //	Create socket using socket function
+    if (listening == -1) { // If socket doesn't exist...
         cerr << "Error creating socket" << endl; // ...Output error message
-        return 1; // ...and return error code
+        return -1; // ...and return error code
     }
-    int port = 54000; // Port specified in client code
-    string ipAddress = "127.0.0.1"; // Local IP address
-    sockaddr_in hint{}; // Initialise hint from the client
+
+    sockaddr_in hint{}; // Initialise hint from the server
     hint.sin_family = AF_INET; // Use IPv4 domain
-    hint.sin_port = htons(port); // Convert port number from host to network short
-    inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr); // Convert IP address to an array of integers
-    int connectRes = connect(sock, (sockaddr *) &hint, sizeof(hint)); // Connect to remote machine
-    if (connectRes == -1) { // If unable to connect...
-        cerr << "Error connecting" << endl; // ...Output error message
-        return 2; // ...and return error code
+    hint.sin_port = htons(54000); // Convert port number from host to network short
+    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr); // Convert IP address to an array of integers
+    bind(listening, (sockaddr *) &hint, sizeof(hint)); // Bind IP address to socket
+    if (::bind(listening, (sockaddr *) &hint, sizeof(hint)) == -1) { // If unable to bind...
+        cerr << "Error binding to port" << endl; // ...Output error message
+        return -2; // ...and return error code
     }
+    listen(listening, SOMAXCONN); // Mark socket for listening
+    if (listen(listening, SOMAXCONN) == -1) { // If unable to listen...
+        cerr << "Error listening to port" << endl; // ...Output error message
+        return -3; // ...and return error code
+    }
+    sockaddr_in client{}; // Initialise client
+    socklen_t clientSize = sizeof(client); // Get size of client
+    int clientSocket = accept(listening, (sockaddr *) &client, &clientSize); // Accept incoming connection
+    char host[NI_MAXHOST]; // Client's remote name
+    char svc[NI_MAXSERV]; // Service (port) that client is connected on
+    memset(host, 0, NI_MAXHOST); // Clean up host memory
+    memset(svc, 0, NI_MAXSERV); // Clean up service memory
+
+    if (clientSocket == -1) { // If unable to accept connection...
+        cerr << "Error connecting to client" << endl; // ...Output error message
+        return -4; // ...and return error code
+    }
+    if (getnameinfo((sockaddr *) &client, sizeof(client), host, NI_MAXHOST, svc, NI_MAXSERV, 0) == 0) { // If it is possible to get remote name
+        cout << host << " connected on port " << svc << endl; // ...Output remote name
+    } else { // Otherwise...
+        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST); // Convert network address to string
+        cout << host << " connected on " << ntohs(client.sin_port) << endl; // Print connection details
+    }
+
+    close(listening); // Close listening socket
+
     char buf[4096]; // Initialise buffer
-    string userInput; // Initialise string for capturing user input
-    do {
-        cout << "> "; // Prompt user for input
-        getline(cin, userInput); // Capture entire line of text
-        int sendRes = send(sock, userInput.c_str(), userInput.size()+1, 0);
-        if (sendRes == -1) { // If unable to send to server...
-            cerr << "Error sending to server" << endl; // ...Output error message
-            continue; // ...and go back to try again
-        }
+
+    while (true) { // While buffer exists, wait for client to send data
         memset(buf, 0, 4096); // Clear buffer
-        int bytesReceived = recv(sock, buf, 4096, 0); // Initialise bytesRecv variable, ready to receive data
-        cout << "Server: " << string(buf, bytesReceived) << "\r\n";
-    } while (true);
-close(sock); // Close socket
+        int bytesRecv = recv(clientSocket, buf, 4096, 0); // Initialise bytesRecv variable, ready to receive data
+        if (bytesRecv == -1) // If there is an error...
+        {
+            cout << "Error receiving data / connection closed" << endl; // ...Output error message
+            break;
+        }
+        if (bytesRecv == 0) { // If no data is received
+            cout << "Client disconnected" << endl; // ...Output error message
+            break;
+        }
+        cout << "Server: " << string(buf, 0, bytesRecv) << endl; // Output message details
+        send(clientSocket, buf, bytesRecv -1, 0); // Echo message back to client
+    }
+    close(clientSocket); // Close client socket
+
+    return 0;
 }
